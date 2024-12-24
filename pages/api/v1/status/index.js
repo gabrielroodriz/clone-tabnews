651158bd8d688fba528fa8fe22c1c2aa5b1084df
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2024-12-24 12:20:44 -0300

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-    response.status(200).json({chave: "acima da média"})
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "acima da média" });
 }
 
 export default status;
