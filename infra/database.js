689deb638ce3ff74a9d4e69c33e6eed5e0c5580e
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-01-12 21:04:48 -0300

    `database` more robust

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+    });
+    try {
+        await client.connect();
+        const result = await client.query(queryObject);
+        return result;
+    } catch (error) {
+        throw new Error("Error querying database", { cause: error });
+    } finally {
+        await client.end();
+    }
 }
 export default {
-  query: query,
+    query: query,
 };
