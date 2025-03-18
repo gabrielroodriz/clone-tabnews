96b873859fcf3c5fe4fd54875e3fe292abba7359
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-03-17 22:56:03 -0300

    Fix `prettier` style linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["*"] },
-    { ignores: [".next", "node_modules", ".swc"] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
+  { files: ["*"] },
+  { ignores: [".next", "node_modules", ".swc"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
 ];
