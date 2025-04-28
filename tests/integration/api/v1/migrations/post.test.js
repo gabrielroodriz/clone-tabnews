144707daff50d4f097e19f642b9e4e6c870e856a
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-04-28 08:38:06 -0300

    refactor: use `orchestrator.cleanDatabase()` in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,45 +1,44 @@
 /* eslint-disable no-undef */
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-    await orchestrator.waitForAllServices();
-    await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
-    describe("Anonymous user", () => {
-        describe("Running pending migrations", () => {
-            it("For the first time", async () => {
-                const response = await fetch(
-                    "http://localhost:3000/api/v1/migrations",
-                    {
-                        method: "POST",
-                    }
-                );
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      it("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
-                expect(response.status).toBe(201);
+        expect(response.status).toBe(201);
 
-                const responseBody = await response.json();
+        const responseBody = await response.json();
 
-                expect(Array.isArray(responseBody)).toBe(true);
-                expect(responseBody.length).toBeGreaterThan(0);
-            });
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
 
-            it("For the second time", async () => {
-                const response = await fetch(
-                    "http://localhost:3000/api/v1/migrations",
-                    {
-                        method: "POST",
-                    }
-                );
-                expect(response.status).toBe(200);
+      it("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(200);
 
-                const responseBody = await response.json();
+        const responseBody = await response.json();
 
-                expect(Array.isArray(responseBody)).toBe(true);
-                expect(responseBody.length).toBe(0);
-            });
-        });
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
     });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,17 +2,17 @@
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-    await orchestrator.waitForAllServices();
+  await orchestrator.waitForAllServices();
 });
 describe("GET to /api/v1/status", () => {
-    let response;
-    let responseBody;
-    it("Retrieving current system status", async () => {
-        response = await fetch("http://localhost:3000/api/v1/migrations");
-        responseBody = await response.json();
+  let response;
+  let responseBody;
+  it("Retrieving current system status", async () => {
+    response = await fetch("http://localhost:3000/api/v1/migrations");
+    responseBody = await response.json();
 
-        expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
 
-        expect(Array.isArray(responseBody)).toBe(true);
-    });
+    expect(Array.isArray(responseBody)).toBe(true);
+  });
 });
