3034f1db35d442271d5de36ca80852a3cee394b2
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-04-28 08:30:09 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,22 +1,18 @@
+/* eslint-disable no-undef */
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices();
+    await orchestrator.waitForAllServices();
 });
-describe("GET to /api/v1/migrations should to return 200", () => {
-  let response;
-  let responseBody;
+describe("GET to /api/v1/status", () => {
+    let response;
+    let responseBody;
+    it("Retrieving current system status", async () => {
+        response = await fetch("http://localhost:3000/api/v1/migrations");
+        responseBody = await response.json();
 
-  beforeEach(async () => {
-    response = await fetch("http://localhost:3000/api/v1/migrations");
-    responseBody = await response.json();
-  });
+        expect(response.status).toBe(200);
 
-  it("should return 200", () => {
-    expect(response.status).toBe(200);
-  });
-
-  it("should responseBody return an array", () => {
-    expect(Array.isArray(responseBody)).toBe(true);
-  });
+        expect(Array.isArray(responseBody)).toBe(true);
+    });
 });
