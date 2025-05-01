5f2c5747a35a6033acc9e2eb50d80d6e85616fea
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-05-01 11:59:59 -0300

    test: rename all descriptions to use new pattern

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,27 +1,35 @@
 import retry from "async-retry";
 
 async function waitForAllServices() {
-  await waitForWebServer();
+    await waitForWebServer();
 
-  async function waitForWebServer() {
-    return retry(fetchStatusPage, {
-      retries: 100,
-      maxTimeout: 2000,
-    });
+    async function waitForWebServer() {
+        return retry(fetchStatusPage, {
+            retries: 100,
+            maxTimeout: 2000,
+        });
 
-    async function fetchStatusPage() {
-      try {
-        const response = await fetch("http://localhost:3000/api/v1/status");
-        if (!response.ok) {
-          throw Error(`HTTP error ${response.status}`);
+        async function fetchStatusPage() {
+            try {
+                const response = await fetch(
+                    "http://localhost:3000/api/v1/status"
+                );
+                if (!response.ok) {
+                    throw Error(`HTTP error ${response.status}`);
+                }
+            } catch (error) {
+                throw Error(error);
+            }
         }
-      } catch (error) {
-        throw Error(error);
-      }
     }
-  }
 }
+
+async function clearDatabase() {
+    await database.query("drop schema public cascade; create schema public;");
+}
+
 const orchestrator = {
-  waitForAllServices,
+    waitForAllServices,
+    clearDatabase,
 };
 export default orchestrator;
