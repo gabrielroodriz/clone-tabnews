bc8e263f9569eb169335e73be0c61b2d6535e602
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2025-06-27 14:41:28 -0300

    fix: lint code

--- infra/errors.js
+++ infra/errors.js
@@ -1,20 +1,19 @@
 export class InternalServerError extends Error {
-    constructor({cause}) {
-        super("Internal server error this is a fallback error", {
-            cause
-        });
-        this.name = "InternalServerError";
-        this.action = "Report this error to the support team";
-        this.statusCode = 500;
-    }
- 
-    toJSON() {
-        return {
-            name: this.name,
-            message: this.message,
-            action: this.action,
-            status_code: this.statusCode,
-        }
-    }
-}
+  constructor({ cause }) {
+    super("Internal server error this is a fallback error", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Report this error to the support team";
+    this.statusCode = 500;
+  }
 
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
