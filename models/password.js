ca3a283cb99510cfcfeed7b1cea3f558750c2e86
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2026-01-12 14:27:35 -0300

    feat: adds a validation to the paramenters of the `compar`e

--- models/password.js
+++ models/password.js
@@ -1,15 +1,18 @@
-import bcryptjs from "bcryptjs";
+import bcryptjs from 'bcryptjs';
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
   return await bcryptjs.hash(password, rounds);
 }
 async function compare(providedPassword, storedPassword) {
+  if (!providedPassword || !storedPassword) {
+    return false;
+  }
   return bcryptjs.compare(providedPassword, storedPassword);
 }
 
 function getNumberOfRounds() {
-  return process.env.NODE_ENV === "production" ? 14 : 1;
+  return process.env.NODE_ENV === 'production' ? 14 : 1;
 }
 const password = {
   hash,
